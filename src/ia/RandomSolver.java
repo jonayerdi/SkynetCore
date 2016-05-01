53284715e38e1c0b86b898d13e52b5c9cd7ec786
jonayerdi <jon_ayerdi_33@hotmail.es>
2016-05-01 22:11:38 +0200

    RandomSolver solve() dividido para reducir complejidad

--- src/ia/RandomSolver.java
+++ src/ia/RandomSolver.java
@@ -6,6 +6,11 @@ import configuration.Logger;
 import database.Recurso;
 
 public class RandomSolver extends Solver {
+	
+	Route route;
+	Recurso recurso;
+	int recursoId;
+	Solution[] sol;
 
 	public RandomSolver(List<String> settings) {
 		super(settings);
@@ -14,27 +19,28 @@ public class RandomSolver extends Solver {
 	private static final long serialVersionUID = 1L;
 	
 	public Solution[] solve() {
-		Route route;
-		Recurso recurso;
-		int recursoId;
-		Solution[] sol = new Solution[incidencias.length];
+		sol = new Solution[incidencias.length];
 		log.log(getClass()+" started -> "+incidencias.length+" incidences / "+recursos.length+" resources", Logger.SOLVER);
 		if(incidencias.length>0 && recursos.length>0) {
 			for(int i = 0 ; i < sol.length ; i++) {
-				try {
-					recursoId = 0;
-					log.log("Solving incidence ID = "+incidencias[i].id+" TYPE = "+incidencias[i].tipo, Logger.SOLVER);
-					while(recursos[recursoId].tipo!=incidencias[i].tipo || recursos[recursoId].estado!=Recurso.ESTADO_LIBRE) recursoId++;
-					recurso = recursos[recursoId];
-					route = new Route(recurso,incidencias[i]);
-					sol[i] = new Solution(incidencias[i],recurso,route);
-					recurso.estado = Recurso.ESTADO_EN_RUTA;
-					log.log("Resource ID = "+recurso.id+" assigned to incidence ID = "+incidencias[i].id+", route takes "+route.getTimeString(), Logger.SOLVER);
-				} catch(Exception e){}
+				resolverIncidencia(i);
 			}
 		}
 		log.log(getClass()+" solved", Logger.SOLVER);
 		return sol;
+	}
+	
+	public void resolverIncidencia(int num) {
+		try {
+			recursoId = 0;
+			log.log("Solving incidence ID = "+incidencias[num].id+" TYPE = "+incidencias[num].tipo, Logger.SOLVER);
+			while(recursos[recursoId].tipo!=incidencias[num].tipo || recursos[recursoId].estado!=Recurso.ESTADO_LIBRE) recursoId++;
+			recurso = recursos[recursoId];
+			route = new Route(recurso,incidencias[num]);
+			sol[num] = new Solution(incidencias[num],recurso,route);
+			recurso.estado = Recurso.ESTADO_EN_RUTA;
+			log.log("Resource ID = "+recurso.id+" assigned to incidence ID = "+incidencias[num].id+", route takes "+route.getTimeString(), Logger.SOLVER);
+		} catch(Exception e){}
 	}
 	
 }
